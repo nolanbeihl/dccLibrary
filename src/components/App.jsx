22d2e7f94349d8d3ffee6f658c05a07961649add
unknown <nolan.beihl@gmail.com>
2021-11-15 14:02:37 -0700

    Completed the footer component, had to trouble shoot error but found it was only an issue with crome and not with the code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import TitleBar from "./TitleBar/TitleBar";
 import BookViewer from "./BookViewer/BookViewer";
 import BookCreator from "./BookCreator/BookCreator";
+import Footer from "./Footer/Footer";
 
 class App extends Component {
     constructor(props){
@@ -54,6 +55,8 @@ render(){
             <TitleBar />
             <BookViewer book={this.books[this.state.booknumber]} nextBook={this.goToNextBook} previousBook={this.goToPreviousBook} />
             <BookCreator createNewBook={this.createBook} />        
+            <Footer />
+            
         </div>
     )
     }
